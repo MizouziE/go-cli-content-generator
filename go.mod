8cd977064b322e160f7b16fd88309051fffbec44
MizouziE <s.anglin@live.co.uk>
2023-02-15 18:26:20 +0100

    add user provides prompt

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.19
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/sashabaranov/go-gpt3 v1.1.0
-	github.com/u-root/u-root v0.11.0
 )
--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 	gogpt "github.com/sashabaranov/go-gpt3"
-	"github.com/u-root/u-root/pkg/cp"
 )
 
 func main() {
+	// Initiate user input reader
+	reader := bufio.NewReader(os.Stdin)
+
 	// Check for .env file
-	if _, err := os.Stat(".env"); err != nil {
-		// Create copy of example file if .env does not
-		cp.Copy(".env.example", ".env")
+	envFileCheck(reader)
+
+	//load env variables - e.g. API Key
+	errLoadDotEnv := godotenv.Load()
+	if errLoadDotEnv != nil {
+		fmt.Println("Error loading .env file\n\nClosing...")
 		return
 	}
 
-	//load env variables - e.g. API Key
-	err := godotenv.Load()
+	// Ask user for prompt
+	fmt.Println("Please provide a prompt:")
+	userPrompt, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error loading .env file\n\nClosing...")
+		fmt.Println("Could not retrieve prompt from user.\n\nClosing...")
 		return
 	}
 
@@ -30,9 +37,9 @@ func main() {
 	ctx := context.Background()
 
 	req := gogpt.CompletionRequest{
-		Model:     gogpt.GPT3Ada,
-		MaxTokens: 5,
-		Prompt:    "Are we connected?",
+		Model:     gogpt.GPT3TextDavinci003,
+		MaxTokens: 100,
+		Prompt:    userPrompt,
 	}
 	resp, err := c.CreateCompletion(ctx, req)
 	if err != nil {
@@ -42,4 +49,44 @@ func main() {
 	// Return answer to prompt
 	fmt.Println(resp.Choices[0].Text)
 
+}
+
+func envFileCheck(r *bufio.Reader) {
+	if _, err := os.Stat(".env"); err != nil {
+		// Create copy of example file if .env does not
+		file, err := os.Create(".env")
+		if err != nil {
+			fmt.Println("Could not create .env file\n\nClosing...")
+		}
+
+		defer file.Close()
+
+		// Ask user to input key
+		fmt.Println("Please enter OpenAI API key below:")
+		apiKey, err := r.ReadString('\n')
+		if err != nil {
+			fmt.Println("Could not retrieve API key.\n\nClosing...")
+			return
+		}
+
+		w := bufio.NewWriter(file)
+		dotEnvInput := "OPEN_AI_API_KEY=" + apiKey
+		_, errWriter := w.WriteString(dotEnvInput)
+		if errWriter != nil {
+			fmt.Println("Failed to write new .env file\n\nClosing...")
+			w.Flush()
+			return
+		}
+
+		w.Flush()
+
+		// Give feedback
+		fmt.Println("API Key added, thank you")
+
+		// Exit
+		return
+	}
+
+	// Greet user
+	fmt.Println("Welcome back!")
 }
