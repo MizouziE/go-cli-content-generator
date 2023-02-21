cb795c04a096f7316fb83bd9ed292c69e208a259
MizouziE <s.anglin@live.co.uk>
2023-02-21 13:40:45 +0100

    refactor WIP

--- main.go
+++ main.go
@@ -26,15 +26,6 @@ type Config struct {
 	Data    string
 }
 
-type Table struct {
-	Headings []string
-	Rows     []map[string]string
-}
-
-type Values struct {
-	Animal, Mood, Description string
-}
-
 func main() {
 	// Initiate user input reader
 	reader := bufio.NewReader(os.Stdin)
@@ -42,132 +33,30 @@ func main() {
 	// Check for .env file
 	envFileCheck(reader)
 
-	fmt.Println("enter config yaml path:")
-	configPath, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Config path no good!\n\nClosing...")
-		return
-	}
-
-	// Read config file
-	configFile, err := os.ReadFile(strings.TrimSpace(configPath))
-	if err != nil {
-		fmt.Println("Unable to read config file\n\nClosing...")
-		return
-	}
-
-	// This will be the config
-	config := Config{}
-
-	err = yaml.Unmarshal(configFile, &config)
-	if err != nil {
-		fmt.Println("Cannot parse config file\n\nClosing...")
-		return
-	}
+	// Read config
+	config := getConfig(reader)
 
 	// Use data path from config to get csv rows
-	csvFromConfig, err := os.Open(strings.TrimSpace(config.Data))
+	csvFile, err := os.Open(strings.TrimSpace(config.Data))
 	if err != nil {
 		fmt.Println("Unable to read csv from config's data path\n\nClosing...")
-		fmt.Printf("Error: %v\n", err)
-		return
+		panic(err)
 	}
 
-	csvFromConfigReader := csv.NewReader(csvFromConfig)
+	csvFileReader := csv.NewReader(csvFile)
 
-	headingRow, err := csvFromConfigReader.Read()
+	headingRow, err := csvFileReader.Read()
 	if err != nil {
 		fmt.Println("Can't read csv with\n\nClosing...")
-		fmt.Println(err)
-		return
+		panic(err)
 	}
 
-	// Make empty list
-	var promptList []string
+	// Build prompt list
+	promptList := setPromptList(csvFileReader, headingRow, config)
 
-	for {
-		row, err := csvFromConfigReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Println("Iteration through csv broken!")
-			return
-		}
+	newDirectoryName := prepareEnvironment()
 
-		substitutes := make(map[string]string)
-
-		for i := range row {
-			substitutes[headingRow[i]] = row[i]
-		}
-
-		for _, prompt := range config.Prompts {
-			// Create a new template
-			tmpl := template.Must(template.New("prompt").Parse(prompt))
-
-			// Execute template
-			promptAsBytes := new(bytes.Buffer)
-			err := tmpl.Execute(promptAsBytes, substitutes)
-			if err != nil {
-				fmt.Println("Cannot execute template\n\nClosing...")
-				fmt.Println(err)
-				return
-			}
-
-			promptList = append(promptList, promptAsBytes.String())
-		}
-	}
-
-	//load env variables - e.g. API Key
-	errLoadDotEnv := godotenv.Load()
-	if errLoadDotEnv != nil {
-		fmt.Println("Error loading .env file\n\nClosing...")
-		return
-	}
-
-	// Connect with API Key
-	c := gogpt.NewClient(os.Getenv("OPEN_AI_API_KEY"))
-	ctx := context.Background()
-
-	// Create directory for responses to prompts
-	newDirectoryName := "storage/" + time.Now().Format("060102_150405")
-	err = os.Mkdir(newDirectoryName, 0777)
-	if err != nil {
-		fmt.Println("Cannot create directory\n\nClosing...")
-		return
-	}
-
-	// Run each prompt from the list thru openai API
-	bar := progressbar.Default(int64((len(promptList))))
-	for index, prompt := range promptList {
-
-		req := gogpt.CompletionRequest{
-			Model:     gogpt.GPT3TextDavinci003,
-			MaxTokens: 300,
-			Prompt:    prompt,
-		}
-		resp, err := c.CreateCompletion(ctx, req)
-		if err != nil {
-			return
-		}
-
-		// Write output to file
-		errWrite := os.WriteFile(strings.TrimSpace(newDirectoryName+"/output-"+strconv.Itoa(index+1)+".md"), []byte(resp.Choices[0].Text), 0777)
-		if errWrite != nil {
-			fmt.Println("Unable to write new file\n\nClosing...\n\nBecause: ", errWrite)
-			return
-		}
-		bar.Add(1)
-	}
-
-	// Save used prompts to same directory
-	listAsJSON, _ := json.Marshal(promptList)
-	JSONfull := `{"prompts":` + string(listAsJSON) + `}`
-	err = os.WriteFile(strings.TrimSpace(newDirectoryName+"/prompts.json"), []byte(JSONfull), 0777)
-	if err != nil {
-		fmt.Println("Unable to save prompts\n\nClosing...")
-		return
-	}
+	runOpenAI(promptList, newDirectoryName)
 }
 
 func envFileCheck(r *bufio.Reader) {
@@ -176,6 +65,7 @@ func envFileCheck(r *bufio.Reader) {
 		file, err := os.Create(".env")
 		if err != nil {
 			fmt.Println("Could not create .env file\n\nClosing...")
+			panic(err)
 		}
 
 		defer file.Close()
@@ -185,7 +75,7 @@ func envFileCheck(r *bufio.Reader) {
 		apiKey, err := r.ReadString('\n')
 		if err != nil {
 			fmt.Println("Could not retrieve API key.\n\nClosing...")
-			return
+			panic(err)
 		}
 
 		w := bufio.NewWriter(file)
@@ -194,7 +84,7 @@ func envFileCheck(r *bufio.Reader) {
 		if errWriter != nil {
 			fmt.Println("Failed to write new .env file\n\nClosing...")
 			w.Flush()
-			return
+			panic(err)
 		}
 
 		w.Flush()
@@ -208,4 +98,128 @@ func envFileCheck(r *bufio.Reader) {
 
 	// Greet user
 	fmt.Println("Welcome back!")
+}
+
+func getConfig(r *bufio.Reader) Config {
+	fmt.Println("enter config yaml path:")
+	configPath, err := r.ReadString('\n')
+	if err != nil {
+		fmt.Println("Config path no good!\n\nClosing...")
+		panic(err)
+	}
+
+	// Read config file
+	configFile, err := os.ReadFile(strings.TrimSpace(configPath))
+	if err != nil {
+		fmt.Println("Unable to read config file\n\nClosing...")
+		panic(err)
+	}
+
+	// This will be the config
+	config := Config{}
+
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		fmt.Println("Cannot parse config file\n\nClosing...")
+		panic(err)
+	}
+
+	return config
+}
+
+func setPromptList(c *csv.Reader, hr []string, config Config) []string {
+	// Make empty list
+	var promptList []string
+
+	for {
+		row, err := c.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Iteration through csv broken!")
+			panic(err)
+		}
+
+		substitutes := make(map[string]string)
+
+		for i := range row {
+			substitutes[hr[i]] = row[i]
+		}
+
+		for _, prompt := range config.Prompts {
+			// Create a new template
+			tmpl := template.Must(template.New("prompt").Parse(prompt))
+
+			// Execute template
+			promptAsBytes := new(bytes.Buffer)
+			err := tmpl.Execute(promptAsBytes, substitutes)
+			if err != nil {
+				fmt.Println("Cannot execute template\n\nClosing...")
+				panic(err)
+			}
+
+			promptList = append(promptList, promptAsBytes.String())
+		}
+
+	}
+	return promptList
+
+}
+
+func prepareEnvironment() string {
+	//load env variables - e.g. API Key
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file\n\nClosing...")
+		panic(err)
+	}
+
+	// Create directory for responses to prompts
+	newDirectoryName := "storage/" + time.Now().Format("060102_150405")
+	err = os.Mkdir(newDirectoryName, 0777)
+	if err != nil {
+		fmt.Println("Cannot create directory\n\nClosing...")
+		panic(err)
+	}
+
+	return newDirectoryName
+}
+
+func runOpenAI(p []string, dir string) {
+	// Connect with API Key
+	c := gogpt.NewClient(os.Getenv("OPEN_AI_API_KEY"))
+	ctx := context.Background()
+
+	// Run each prompt from the list thru openai API
+	bar := progressbar.Default(int64((len(p))))
+	for index, prompt := range p {
+
+		req := gogpt.CompletionRequest{
+			Model:     gogpt.GPT3TextDavinci003,
+			MaxTokens: 300,
+			Prompt:    prompt,
+		}
+		resp, err := c.CreateCompletion(ctx, req)
+		if err != nil {
+			return
+		}
+
+		// Write output to file
+		err = os.WriteFile(strings.TrimSpace(dir+"/output-"+strconv.Itoa(index+1)+".md"), []byte(resp.Choices[0].Text), 0777)
+		if err != nil {
+			fmt.Println("Unable to write new file\n\nClosing...\n\nBecause: ", err)
+			return
+		}
+		bar.Add(1)
+	}
+
+	// Save used prompts to same directory
+	listAsJSON, _ := json.Marshal(p)
+	JSONfull := `{"prompts":` + string(listAsJSON) + `}`
+	err := os.WriteFile(strings.TrimSpace(dir+"/prompts.json"), []byte(JSONfull), 0777)
+	if err != nil {
+		fmt.Println("Unable to save prompts\n\nClosing...")
+		return
+	}
 }
